ff4eb8b92445a20407648cd88e2571f34d6221a1
James Karki <karkijames07@gmail.com>
2026-03-19 16:18:54 +0545

    Under development

--- backend/auth/pathname.php
+++
@@ -1,3 +0,0 @@
-<?php 
-echo realpath("adminDashboard.html");
-?>
--- public/assets/js/test.js
+++
@@ -1,8 +0,0 @@
-const object = {
-    a:'some string', 
-    b: 42,
-}
-
-Object.keys(object).forEach(key=>{
-    console.log(key);
-})
